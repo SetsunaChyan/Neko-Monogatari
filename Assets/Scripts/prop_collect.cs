72306d101b235827b9b92e87a4280f4ba05e4d99
HiedanoAkyuu <ctzguozi@163.com>
2019-07-07 02:06:20 +0800

    好耶 捡星星

---
+++ Assets/Scripts/prop_collect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prop_collect : MonoBehaviour 
+{
+	public int probtype;
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		cat_controller cat=other.GetComponent<cat_controller>();
+		if(cat==null) return;
+		if(probtype==0) cat.currentHeart++;
+		else cat.currentTri++;
+		Destroy(gameObject);
+	}
+}
