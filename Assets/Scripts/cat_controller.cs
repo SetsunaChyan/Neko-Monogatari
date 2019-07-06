3bb0fc6efc0a38ba48e8d7243c4692c2b8dedd22
HiedanoAkyuu <ctzguozi@163.com>
2019-07-06 17:46:52 +0800

    好耶 能推箱子了

--- Assets/Scripts/cat_controller.cs
+++ Assets/Scripts/cat_controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class cat_controller : MonoBehaviour 
 {
@@ -13,6 +14,9 @@ public class cat_controller : MonoBehaviour
 	private float GridHeight;
 
 	private Rigidbody2D rigidbody2d;
+
+	public Tilemap tilemap;
+	public Tile boxTile;
 
 	void Start()
 	{
@@ -38,15 +42,41 @@ public class cat_controller : MonoBehaviour
 		else if(direction==RIGHT) transform.localScale=new Vector3(-1,1,1);
 
 		//判断前方是否有互动
-		Vector2 vec_detect=new Vector2(-1,0);
-		if(keyPressed==RIGHT) vec_detect=new Vector2(1,0);
+		Vector2 vec_detect=new Vector2(0,0);
+		if(keyPressed==LEFT) vec_detect=new Vector2(-1,0);
+		else if(keyPressed==RIGHT) vec_detect=new Vector2(1,0);
 		else if(keyPressed==UP) vec_detect=new Vector2(0,1);
 		else if(keyPressed==DOWN) vec_detect=new Vector2(0,-1);
+
+		if(Physics2D.Raycast(rigidbody2d.position,vec_detect,GridHeight,1<<LayerMask.NameToLayer("Collider"))) 
+		{
+    		GameObject obj=Physics2D.Raycast(rigidbody2d.position,vec_detect,GridHeight,1<<LayerMask.NameToLayer("Collider")).collider.gameObject;
+			if(obj.name=="Box")
+			{
+				bool flag=true;
+				Vector3Int cellPosition1=tilemap.WorldToCell(rigidbody2d.position+vec_detect);
+				Vector3Int cellPosition2=tilemap.WorldToCell(rigidbody2d.position+2*vec_detect);
+				Vector2 cellPosition1_2d=new Vector2(0,0);
+				cellPosition1_2d.x+=cellPosition1.x;cellPosition1_2d.y+=cellPosition1.y;
+				if(Physics2D.Raycast(cellPosition1_2d,vec_detect,GridHeight,1<<LayerMask.NameToLayer("Collider")))
+				{
+					TileBase tb2=tilemap.GetTile(cellPosition2);
+					if(tb2!=null) flag=false;
+				}
+				TileBase tb1=tilemap.GetTile(cellPosition1);
+				if(tb1!=null&&flag)
+				{
+					tilemap.SetTile(cellPosition1,null);
+					tilemap.SetTile(cellPosition2,tb1);
+					rigidbody2d.MovePosition(rigidbody2d.position+vec_detect);
+				} 
+			}
+		}
 
 		//判断前方是否有障碍物
 		if(keyPressed!=-1)
 		{
-			//如果没哟障碍物则移动
+			//如果没有障碍物则移动
 			if(!Physics2D.Raycast(rigidbody2d.position,vec_detect,GridHeight,1<<LayerMask.NameToLayer("Collider"))) 
 			{
         		Vector2 newpos=rigidbody2d.position+vec_detect;
